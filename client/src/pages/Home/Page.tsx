1b37556378b8297b078faeaea11b7a9c734a5ce7
Ayush <ayushshivhare51@gmail.com>
2025-04-08 15:28:23 +0530

    chore: shadcn and hotel cards

--- client/src/pages/Home/Page.tsx
+++ client/src/pages/Home/Page.tsx
@@ -1,8 +1,33 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import hotels from "@/../public/hotes.json";
+import { Button } from "@/components/ui/button";
+
 export default function Page() {
   return (
-    <div className="h-screen">
-      <h1>Home</h1>
-      <p>Welcome to the home page!</p>
+    <div className="grid grid-cols-3 h-screen">
+      {hotels.value.map((hotel) => (
+        <div key={hotel.HotelId} className=" border border-black p-4">
+          <Card>
+            <CardHeader>
+              <CardTitle>{hotel.HotelName}</CardTitle>
+              <CardDescription>{hotel.Description}</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p>Hotel Content</p>
+            </CardContent>
+            <CardFooter>
+              <Button variant="default">Book now</Button>
+            </CardFooter>
+          </Card>
+        </div>
+      ))}
     </div>
   );
 }
